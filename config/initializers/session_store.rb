a786858fb6f23094fcf351b6c40d565088096398
jordanhudgens <jordan@devcamp.com>
2019-03-13 15:55:25 -0500

    Added check for production env in the session store initializer

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "jdh-authentication-app-api.herokuapp.com"
+if Rails.env == 'production'
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "jdh-authentication-app-api.herokuapp.com"
+else
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
